acfa8330ab959b33405c478511ceccdaee35b24f
Antoine <sirpa.antoine@gmail.com>
2020-12-20 18:01:47 +0100

    create methods api

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * get all users
+     */
+    public function getAllUser()
+    {
+        
+    }
+    
+    /**
+     * get one user by id
+     */
+    public function getOneUser($id)
+    {
+        
+    }
+    
+}
